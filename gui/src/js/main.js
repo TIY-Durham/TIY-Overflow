603cbf208a1138de380f8cfa9409dd44bf04d23b
David Rogers <al.the.x@gmail.com>
2015-10-26 12:52:08 -0400

    Define and attach dummy data via literals in a controller

--- gui/src/js/main.js
+++ gui/src/js/main.js
@@ -6,6 +6,17 @@
       })
       .when('/questions', {
         templateUrl: 'partials/question-list.html',
+        controller: [ '$scope', '$http', function($scope, $http){
+          $scope.all = [
+            { question_id: 123,
+              question_title: 'This is the title!',
+              question_body: 'What is the meaning of life, the universe, and everything?',
+              asker: 'username',
+              created_on: String(new Date),
+              modified_on: String(new Date),
+            }
+          ];
+        } ],
       })
       .when('/questions/1', {
         templateUrl: 'partials/question-detail.html',
